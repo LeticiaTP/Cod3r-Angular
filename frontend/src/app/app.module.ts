3082644cdcc643174c6e8dca5b42c3cc3cd19d1b
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-06-16 19:04:01 -0300

    Correção do erro de import

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,7 +25,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -40,7 +39,6 @@ import { MatSortModule } from '@angular/material/sort';
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
